d02a3c01077d9dd85fe61245f8ab2eee0b08d78d
Jermaine <jermaine.aroma@gmail.com>
2024-02-13 21:52:49 +0100

    Updating state with button using a setter function for name and age.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import React, { useState } from 'react';
+
+const createInitialState = () => {
+    const age = new Date().getFullYear() - 1992;
+    return age;
+};
+
 const App = () => {
-	return (
-		<div className="App">
-			<h1>React Hooks Exercise Starter</h1>
-		</div>
-	);
+  const [name, setName] = useState('Jermaine');
+  const [age, setAge] = useState(createInitialState());
+
+  const handleClick = () => {
+    setName((prevName) => prevName + 'o');
+    setName((prevName) => prevName + 'o');
+
+    console.log(name); 
+  };
+
+  const handleBirthday = () => {
+    setAge((prevAge) => prevAge + 1);
+  };
+
+  return (
+    <div className="App">
+      <h1>React Hooks Exercise Starter</h1>
+      <p>My name is {name}</p>
+      <p>My age is {age}</p>
+      <button onClick={handleClick}>Add &apos;o&apos;</button>
+      <button onClick={handleBirthday}>Celebrate Birthday</button>
+    </div>
+  );
 };
 
 export default App;
